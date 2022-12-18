6c3e7049239423f2f9c6a3f975ab8b9128ea0c54
WIN-CF9BAEAH6EV\user <zuev.f.s@gmail.com>
2022-12-18 20:48:53 +1000

    Add UserController Functionality

---
+++ Hospital/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Entities;
+using Domain.Services;
+using Hospital.View;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _service;
+
+        public UserController(UserService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult<UserSearchView> GetUserByLogin(string login)
+        {
+            if (string.IsNullOrEmpty(login))
+            {
+                return Problem(statusCode: 404, detail: "Не указан логин");
+            }
+
+            var userRes = _service.GetUserByLogin(login);
+            if(userRes.IsFailure)
+            {
+                return Problem(statusCode: 404, detail: userRes.Error);
+            }
+
+            return Ok(new UserSearchView
+            {
+                UserId = userRes.Value.UserId,
+                Login = userRes.Value.Login,
+                Password = userRes.Value.Password,
+                PhoneNumber= userRes.Value.PhoneNumber,
+                Initials= userRes.Value.Initials,
+                Role= userRes.Value.Role
+            });
+        }
+
+        public ActionResult<UserSearchView> CreateUser(UserSearchView userView)
+        {
+            if (string.IsNullOrEmpty(userView.Login))
+                return Problem(statusCode: 404, detail: "Не указан логин");
+            if (string.IsNullOrEmpty(userView.Password))
+                return Problem(statusCode: 404, detail: "Не указан пароль");
+
+            User user = new User(userView.UserId, userView.Login, userView.Password, userView.PhoneNumber, userView.Initials, userView.Role);
+            var userRes = _service.CreateUser(user);
+            if (userRes.IsFailure)
+            {
+                return Problem(statusCode: 404, detail: userRes.Error);
+            }
+
+            return Ok(userView);
+        }
+
+        public ActionResult CheckUser(string login, string password)
+        {
+            if (string.IsNullOrEmpty(login))
+                return Problem(statusCode: 404, detail: "Не указан логин");
+            if (string.IsNullOrEmpty(password))
+                return Problem(statusCode: 404, detail: "Не указан пароль");
+
+            var userRes = _service.CheckUser(login, password);
+            if (userRes.IsFailure)
+            {
+                return Problem(statusCode: 404, detail: userRes.Error);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Hospital/View/UserSearchView.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System.Text.Json.Serialization;
+
+namespace Hospital.View
+{
+    public class UserSearchView
+    {
+        [JsonPropertyName("id")]
+        public int UserId { get; set; }
+        [JsonPropertyName("login")]
+        public string Login { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("phonenumber")]
+        public string PhoneNumber { get; set; }
+        [JsonPropertyName("initials")]
+        public string Initials { get; set; }
+        [JsonPropertyName("role")]
+        public UserRole Role { get; set; }
+    }
+}
