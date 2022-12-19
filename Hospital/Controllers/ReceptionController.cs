c48f7be434078197856aab6993397234f6b60a63
WIN-CF9BAEAH6EV\user <zuev.f.s@gmail.com>
2022-12-19 12:14:10 +1000

    Create Reception View Functionality

---
+++ Hospital/Controllers/ReceptionController.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Entities;
+using Domain.Services;
+using Hospital.View;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class ReceptionController : Controller
+    {
+        private readonly ReceptionService _service;
+        public ReceptionController(ReceptionService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult<ReceptionSearchView> SaveAppointment(DateTime start, DateTime end)
+        {
+            var res = _service.SaveAppointment(start, end);
+            if(res.IsFailure)
+            {
+                return Problem(statusCode: 404, detail: "Не удалось сохранить");
+            }
+            ReceptionSearchView view = new ReceptionSearchView()
+            {
+                StartTime = start,
+                EndTime = end,
+                DoctorId = res.Value.DoctorId,
+                UserId = res.Value.UserId
+            };
+            return Ok(view);
+        }
+
+        public ActionResult<ReceptionSearchView> SaveAppointment(DateTime start, DateTime end, DoctorSearchView docView)
+        {
+            Doctor doctor = new Doctor(docView.DoctorId, docView.Initials, docView.SpecializationId);
+            var res = _service.SaveAppointment(start, end, doctor);
+            if (res.IsFailure)
+            {
+                return Problem(statusCode: 404, detail: "Не удалось сохранить");
+            }
+            ReceptionSearchView view = new ReceptionSearchView()
+            {
+                StartTime = start,
+                EndTime = end,
+                DoctorId = res.Value.DoctorId,
+                UserId = res.Value.UserId
+            };
+            return Ok(view);
+        }
+
+        public ActionResult<List<DateTime>> GetFreeAppointmentDateList(int specializationId)
+        {
+            var res = _service.GetFreeAppointmentDateList(specializationId);
+            if(res.Value.Count == 0)
+            {
+                return Problem(statusCode:404, detail:"Не удалось получить");
+            }
+            return Ok(res.Value);
+        }
+    }
+}
---
+++ Hospital/View/ReceptionSearchView.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hospital.View
+{
+    public class ReceptionSearchView
+    {
+        [JsonPropertyName("starttime")]
+        public DateTime StartTime { get; set; }
+        [JsonPropertyName("endtime")]
+        public DateTime EndTime { get; set; }
+        [JsonPropertyName("userid")]
+        public int UserId { get;set; }
+        [JsonPropertyName("doctorid")]
+        public int DoctorId { get; set; }
+    }
+}
